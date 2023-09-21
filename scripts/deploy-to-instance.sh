890f140a716a95911d71c2f4eb0b34afd60570f6
Jake Siney <jakesiney@gmail.com>
2023-09-21 12:24:17 +0100

    First ci attempt

---
+++ scripts/deploy-to-instance.sh
@@ -0,0 +1,24 @@
+sudo apt-get update -y
+sudo apt-get install -y openssh-client rsync
+echo "test"
+
+echo $INSTANCE_IP
+echo $AWS_CLI_TOKEN
+echo $AWS_SECRET_TOKEN
+
+eval $(ssh-agent -s)
+echo "$POOCHIE_TOKEN" | tr -d '\r' | ssh-add -
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+
+# we should probably avoid hardcoding this, in case
+# the IP changes
+rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@$INSTANCE_IP:/var/poochiepals/
+# rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@$INSTANCE_IP_TWO:/var/poochiepals/
+
+echo "rsync finished"
+ssh -o StrictHostKeyChecking=no ec2-user@$INSTANCE_IP "sudo systemctl restart poochiepals"
+ssh -o StrictHostKeyChecking=no ec2-user@$INSTANCE_IP_TWO "sudo systemctl restart poochiepals"
+echo "reload finished"
+
